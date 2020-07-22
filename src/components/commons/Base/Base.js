e3e4c240ffe4315f8b29d6b5130237ee73566d55
David Mariscal <david.mariscal.fdez@hotmail.com>
2020-07-21 20:04:16 -0500

    third test

--- src/components/commons/Base/Base.js
+++ src/components/commons/Base/Base.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+//import { Route } from 'react-router-dom';
 import CreateBlog from '../../views/Blog/New/CreateBlog';
 import Blog from '../../views/Blog/View/Blog';
 import Home from '../../views/Home/Home';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 export default class Base extends Component {
     render() {
@@ -10,9 +12,13 @@ export default class Base extends Component {
             <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <div className="App">
         <header className="App-header">
+            <Router>
+            <div>
           <Route exact path="/" component={Home} />
           <Route path="/blog/new" component={CreateBlog} />
           <Route path="/blog" component={Blog} />
+          </div>
+          </Router>
         </header>
         </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import firebase from 'firebase';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyA0YLy3BIf4ph0va_6AurvbUPHVaTTSvtw",
-//   authDomain: "hex-blog-backend-994ab.firebaseapp.com",
-//   databaseURL: "https://hex-blog-backend-994ab.firebaseio.com",
-//   projectId: "hex-blog-backend-994ab",
-//   storageBucket: "hex-blog-backend-994ab.appspot.com",
-//   messagingSenderId: "898102284917",
-//   appId: "1:898102284917:web:ce5f2744b2ae61184d5e3c",
-//   measurementId: "G-J4S5GRLWV3"
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyA0YLy3BIf4ph0va_6AurvbUPHVaTTSvtw",
+  authDomain: "hex-blog-backend-994ab.firebaseapp.com",
+  databaseURL: "https://hex-blog-backend-994ab.firebaseio.com",
+  projectId: "hex-blog-backend-994ab",
+  storageBucket: "hex-blog-backend-994ab.appspot.com",
+  messagingSenderId: "898102284917",
+  appId: "1:898102284917:web:ce5f2744b2ae61184d5e3c",
+  measurementId: "G-J4S5GRLWV3"
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
