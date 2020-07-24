83e7ede3e85a17e39ebf899be36a4dff7bade1de
David Mariscal <david.mariscal.fdez@hotmail.com>
2020-07-24 03:07:18 -0500

    corrected conditionals

--- src/components/views/Blog/View/Blog.js
+++ src/components/views/Blog/View/Blog.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import React from 'react';
 //import axios from 'axios';
-import { Card, Typography, Row } from 'antd';
+import { Typography } from 'antd';
 
 const { Title } = Typography
 //const { Meta } = Card;
