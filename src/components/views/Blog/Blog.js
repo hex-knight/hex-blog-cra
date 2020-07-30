1f639dd7952380077251e981467077567aa274a2
David Mariscal <david.mariscal.fdez@hotmail.com>
2020-07-30 03:30:03 -0500

    Implementation of Blog page per Id, styling and fixed embed videos styling

--- src/components/views/Blog/Blog.js
+++ src/components/views/Blog/Blog.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-//import Title from 'antd/lib/skeleton/Title';
 import "../../../../node_modules/react-quill/dist/quill.core.css";
 
 export default class Blog extends Component {
@@ -30,8 +29,7 @@ export default class Blog extends Component {
                 this.setState({ fetching: false })
             } else {
                 let Post = snapshot.val()
-                //console.log(Post);
-                //const renderBlog = 
+                 
                 this.setState({ result: Post })
                 setTimeout(
                     this.setState({ fetching: false }),
@@ -46,9 +44,7 @@ export default class Blog extends Component {
 
     render() {
         return (
-            // <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
             <div className="postPage" style={{color:'#4b4b4b'}}>
-                {/* <Col xs={25} md={24} lg={12} xl={8}> */}
                 {this.state.fetching ?
                     (<h6>Cargando...</h6>) :
                     (
@@ -75,29 +71,8 @@ export default class Blog extends Component {
                         </div>
                     )
                 }
-                {/* </Col> */}
             </div>
-            // </Row>
         )
     }
 }
 
-
-// <div className="postPage" style={{ width: '80%', margin: '3rem auto' }}>
-                    //     <Title level={2}>
-                    //         {Post.titulo!==''? 
-                    //         Post.titulo :
-                    //         Post.fecha
-                    //         }
-                    //         </Title>
-                    //     <br />
-                    //     {
-                    //         Post.titulo!==''?
-                    //         (
-                    //             <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    //         <Title level={4}>{Post.fecha}</Title>
-                    //     </div>
-                    //         ):null
-                    //     }
-                    //     <div dangerouslySetInnerHTML={{ __html: Post.contenido }} />
-                    // </div>
